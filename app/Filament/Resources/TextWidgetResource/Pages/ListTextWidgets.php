8d4b2c761bdcab5bc4c42cc4a7e8a912aecfbc39
ur7ez <ur7ez@ukr.net>
2023-07-31 17:28:26 +0300

    backend: add text widgets feature

---
+++ app/Filament/Resources/TextWidgetResource/Pages/ListTextWidgets.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\TextWidgetResource\Pages;
+
+use App\Filament\Resources\TextWidgetResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListTextWidgets extends ListRecords
+{
+    protected static string $resource = TextWidgetResource::class;
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
