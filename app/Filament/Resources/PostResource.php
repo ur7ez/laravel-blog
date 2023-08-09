716d071d943b4dab7a0e998ed8e5fa66a8e03875
ur7ez <ur7ez@ukr.net>
2023-08-09 15:10:20 +0300

    admin: add post view page; add PostOverview widget there to display stats of post views, up/down votes

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -81,6 +81,7 @@ class PostResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
@@ -101,6 +102,7 @@ class PostResource extends Resource
         return [
             'index' => Pages\ListPosts::route('/'),
             'create' => Pages\CreatePost::route('/create'),
+            'view' => Pages\ViewPost::route('/{record}'),
             'edit' => Pages\EditPost::route('/{record}/edit'),
         ];
     }
---
+++ app/Filament/Resources/PostResource/Pages/ViewPost.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\PostResource\Pages;
+
+use App\Filament\Resources\PostResource;
+use App\Filament\Widgets\PostOverview;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewPost extends ViewRecord
+{
+    protected static string $resource = PostResource::class;
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    protected function getActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PostOverview::class
+        ];
+    }
+}
