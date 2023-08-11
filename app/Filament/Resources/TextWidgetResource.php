b47885f898845465d96ce49034eae4f51a2123bc
ur7ez <ur7ez@ukr.net>
2023-08-11 14:48:47 +0300

    clear code

--- app/Filament/Resources/TextWidgetResource.php
+++ app/Filament/Resources/TextWidgetResource.php
@@ -10,8 +10,6 @@ use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class TextWidgetResource extends Resource
 {
